181fc57b0ad559ce26937544673c18c7714ee0a7
salsafadhilah <37093417+salsafadhilah@users.noreply.github.com>
2018-03-20 10:23:30 +0700

    Add files via upload

---
+++ input.output(salsa).py
@@ -0,0 +1,18 @@
+print("                   Input dan Output")
+print("================================================== \n")
+
+nm = raw_input("Nama :")
+ttl=input("Ttl  :")
+jk=raw_input("Jenis Kelamin :")
+kw= raw_input("Kewarganegaraan :")
+almt=raw_input("Alamat :")
+
+print"\n\n"
+
+print "   *** IDENTITY ***\n"
+
+print (nm)
+print(ttl)
+print(jk)
+print(kw)
+print(almt)
